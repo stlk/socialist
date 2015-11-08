461b014daea51123544ba4a952d70f66748d5d53
Josef Rousek <josef.rousek@gmail.com>
2015-11-08 11:58:25 +0100

    Add option to send newsletter to a single user

--- console/management/commands/send_newsletter.py
+++ console/management/commands/send_newsletter.py
@@ -4,6 +4,17 @@ from console.newsletter import Newsletter
 class Command(BaseCommand):
     help = 'Sends newsletter to all subscribed users'
 
+    def add_arguments(self, parser):
+        parser.add_argument('user',
+            nargs='?',
+            type=int,
+            default=None,
+            help='Send newsletter to a single user')
+
     def handle(self, *args, **options):
-        Newsletter().send_to_all_subscribers()
-        self.stdout.write('Successfully started generating newsletter')
+        if options['user']:
+            user = Newsletter().send_to_one_subscriber(options['user'])
+            self.stdout.write('Successfully started generating newsletter for: %s' % user.username)
+        else:
+            users = Newsletter().send_to_all_subscribers()
+            self.stdout.write('Successfully started generating newsletter for %d users' % len(users))
--- console/newsletter.py
+++ console/newsletter.py
@@ -9,6 +9,14 @@ class Newsletter():
         send_recommendations.delay(user)
 
     def send_to_all_subscribers(self):
-        for user in User.objects.filter(subscription__id__isnull=False, subscription__cancelled=None):
+        users = User.objects.filter(subscription__id__isnull=False, subscription__cancelled=None)
+        for user in users:
             logging.info('Sending newsletter to %s', user.username)
             self.send(user)
+        return users
+
+    def send_to_one_subscriber(self, user_id: int):
+        user = User.objects.filter(subscription__id__isnull=False, subscription__cancelled=None).get(id=user_id)
+        logging.info('Sending newsletter to %s', user.username)
+        self.send(user)
+        return user
