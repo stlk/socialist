1f40801f1a723311dbe13500cace28542869a37a
Josef Rousek <josef.rousek@gmail.com>
2015-12-22 14:04:31 +0100

    Copy like-based recommendation to engine

--- recommendation_engine/tests.py
+++ recommendation_engine/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from unittest.mock import patch, Mock, PropertyMock
+from instagram.client import InstagramAPI
+from .instagram import Instagram
 
-# Create your tests here.
+
+class MockInstagramAPI(InstagramAPI):
+
+    def __init__(self, access_token, client_secret):
+        tag = Mock()
+        type(tag).name = PropertyMock(return_value='tag')
+        user = Mock()
+        type(user).username = PropertyMock(return_value='username')
+        medium = Mock(id=1,
+                      like_count=1,
+                      link='',
+                      get_standard_resolution_url=Mock(return_value=''),
+                      user=user)
+        type(medium).tags = PropertyMock(return_value=[tag])
+
+        self.medium = medium
+
+    def user_liked_media(self, with_next_url=None):
+        return [self.medium], None
+
+    def tag_recent_media(self, tag_name, with_next_url=None):
+        return [self.medium], None
+
+
+@patch('django.contrib.auth.models.User')
+@patch('recommendation_engine.instagram.InstagramAPI', new=MockInstagramAPI)
+class InstagramTest(TestCase):
+
+    def test_recommend_returns_correct_data(self, User):
+        user = User()
+
+        instagram = Instagram(user)
+        recommendations = instagram.recommend()
+
+        self.assertEqual(recommendations[0]['tags'][0], 'tag')
